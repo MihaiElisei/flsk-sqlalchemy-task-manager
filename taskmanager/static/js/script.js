9b858ba7e541fed679808d48f606e05fe5d7509a
MihaiElisei <mihai.m.elisei@gmail.com>
2022-10-23 13:44:23 +0000

    add materialize nav and footer

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,6 @@
+// Initialization for navbar
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
